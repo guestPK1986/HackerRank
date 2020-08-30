d2f22fc0d6bde2fd07bad5b9c347d472e390d910
labsSQA <kps090111@gmail.com>
2020-08-30 17:11:06 -0500

    Create Function_average.py

---
+++ Function_average.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
+    nums = list(map(int, input().split()))
+    sumOfNumbers = 0
+    for x in nums:
+        sumOfNumbers = sumOfNumbers + x
+        avg = sumOfNumbers / len(nums)    
+    fptr.write('%.2f' % avg + '\n')
+    fptr.close()
